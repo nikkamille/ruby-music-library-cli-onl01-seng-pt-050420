4f54144aaf16761e8e66c6fd8fd3c3f407307219
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-16 03:23:01 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -39,7 +39,7 @@ class MusicLibraryController
   end
   
   def list_songs 
-    Song.all.sort { |a, b| a.name <=> b.name }.each_with_index(1) do |song, index|
+    Song.all.sort { |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
